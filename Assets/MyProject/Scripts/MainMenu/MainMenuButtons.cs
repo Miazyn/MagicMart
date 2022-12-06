d9c70fb0c3fcdf930bf38ff6c76d79885810023a
Miazyn <mikeymillamf@gmail.com>
2022-12-06 01:36:29 +0100

    Finish day 1

---
+++ Assets/MyProject/Scripts/MainMenu/MainMenuButtons.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    [SerializeField] string mainGame = "";
+    
+    public void StartGame()
+    {
+        SceneManager.LoadScene(mainGame, LoadSceneMode.Single);
+    }
+    public void QuitApplication()
+    {
+       
+        QuitApplication();
+    }
+
+
+}
