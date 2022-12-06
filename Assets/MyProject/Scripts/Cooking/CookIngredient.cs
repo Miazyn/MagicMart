5fa14ed4f289e3595ed92aed266d477bfa312bc9
Miazyn <mikeymillamf@gmail.com>
2022-12-06 09:44:11 +0100

    Can drop at Kessel

--- Assets/MyProject/Scripts/Cooking/CookIngredient.cs
+++ Assets/MyProject/Scripts/Cooking/CookIngredient.cs
@@ -2,32 +2,53 @@ using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class CookIngredient : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler, IPointerDownHandler
+public class CookIngredient : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler, IPointerDownHandler, IDropHandler
 {
     RectTransform rect;
+    Vector2 originalPosition;
+    [SerializeField] Canvas canvas;
+    [SerializeField] CanvasGroup canvasGroup;
+    [SerializeField] float onDragAlpha;
     private void Awake()
     {
         rect = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+        originalPosition = rect.anchoredPosition;
+        if (!canvas)
+        {
+            Debug.LogWarning("NO CANVAS FOR SCALE DEFINED");
+        }
     }
     public void OnBeginDrag(PointerEventData eventData)
     {
-        Debug.Log("BeginDrag");
-
+        Debug.Log("OnBeginDrag");
+        canvasGroup.alpha = onDragAlpha;
+        canvasGroup.blocksRaycasts = false;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        rect.anchoredPosition += eventData.delta;
+        rect.anchoredPosition += eventData.delta / canvas.scaleFactor;
         Debug.Log("Dragging");
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        Debug.Log("EndDrag");
+        Debug.Log("OnEndDrag");
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        Debug.Log("Pointer down");
+      //  Debug.Log("Pointer down");
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("Item has been dropped");
+        // Back to original position
+       // rect.anchoredPosition = originalPosition;
     }
 }
