f33b48a35b98dc51daba01987424fe0fc315d160
Miazyn <mikeymillamf@gmail.com>
2022-12-06 09:49:41 +0100

    Item can be placed in Kessel

--- Assets/MyProject/Scripts/Cooking/CookingPot.cs
+++ Assets/MyProject/Scripts/Cooking/CookingPot.cs
@@ -8,5 +8,10 @@ public class CookingPot : MonoBehaviour, IDropHandler
     public void OnDrop(PointerEventData eventData)
     {
         Debug.Log("Dropping at: Script from: " +  name);
+        if(eventData.pointerDrag != null)
+        {
+            Debug.Log(GetComponent<RectTransform>().anchoredPosition);
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+        }
     }
 }
